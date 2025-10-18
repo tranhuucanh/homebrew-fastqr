3509cc27ec597294a6dce19a722d47ac21ab64ef
Tran Huu Canh <canh.th@raksul.com>
2025-10-18 20:25:02 +0700

    Update to v1.0.1 - Fix memory corruption in QR generation

--- Formula/fastqr.rb
+++ Formula/fastqr.rb
@@ -1,17 +1,17 @@
 class Fastqr < Formula
   desc "Lightning-fast QR code generator with advanced features"
   homepage "https://github.com/tranhuucanh/fastqr"
-  version "1.0.0"
+  version "1.0.1"
   license "LGPL-2.1"
 
   on_arm do
-    url "https://github.com/tranhuucanh/fastqr/releases/download/v1.0.0/fastqr-1.0.0-macos-arm64.tar.gz"
-    sha256 "fa83d4a70a3a4c449598ec94171e61b9430d409407084a61810dea90328fef5b"
+    url "https://github.com/tranhuucanh/fastqr/releases/download/v1.0.1/fastqr-1.0.1-macos-arm64.tar.gz"
+    sha256 "1b7d408066d5e0e2f231b90c61d1581f89c2ab6cddf5b9b76a23b96c478a34b0"
   end
 
   on_intel do
-    url "https://github.com/tranhuucanh/fastqr/releases/download/v1.0.0/fastqr-1.0.0-macos-x86_64.tar.gz"
-    sha256 "cf799762eba8980358348ca45405ef10b7eed3fab9d7eca7d45dbd0330e2d718"
+    url "https://github.com/tranhuucanh/fastqr/releases/download/v1.0.1/fastqr-1.0.1-macos-x86_64.tar.gz"
+    sha256 "8d18b3617fbc52aa19a8cca196d9fe7bee2ed58a60ce6ebace0d24c7fa0aaf47"
   end
 
   depends_on "vips"
@@ -21,18 +21,18 @@ class Fastqr < Formula
   def install
     # Homebrew auto-strips single top-level directory from tarball
     # So files are directly in buildpath: bin/, lib/, include/
-    
+
     # Install library first
     lib.install "lib/libfastqr.dylib"
     # Create version symlink
     ln_s lib/"libfastqr.dylib", lib/"libfastqr.1.dylib"
-    
+
     # Install binary
     bin.install "bin/fastqr"
-    
+
     # Install headers
     include.install "include/fastqr.h" if File.exist?("include/fastqr.h")
-    
+
     # Fix rpath to use installed library (after files are installed)
     system "install_name_tool", "-change", "@rpath/libfastqr.1.dylib",
            "#{lib}/libfastqr.1.dylib", bin/"fastqr"
